169f98cd52ef300818b470ff7c0d3210942fa473
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-06-17 23:51:49 +0800

    feat: ✨ add scheduledDeletionAt to MinecraftProfile

--- src/main/java/org/bsrserver/altair/velocity/data/MinecraftProfile.java
+++ src/main/java/org/bsrserver/altair/velocity/data/MinecraftProfile.java
@@ -2,10 +2,12 @@ package org.bsrserver.altair.velocity.data;
 
 import lombok.Data;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
 public class MinecraftProfile {
     private Integer accountId;
     private UUID uuid;
+    private Instant scheduledDeletionAt;
 }
